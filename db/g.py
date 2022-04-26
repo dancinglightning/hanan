8ba1f160aa4ca927da180641eb074f1db85dc164
Hanan Basheer <20b030018@iitb.ac.in>
2022-04-26 21:13:29 +0530

    Final

--- db/g.py
+++ db/g.py
@@ -2,18 +2,20 @@ def minCostConnectPoints(points) -> int:
         x = 0
         d = []
         for i in range(len(points)):
-            if i not in d:
-                y = float("inf")
-                k = 0
-                for j in range(len(points)):
-                    if i!=j:
-                        y = min(y, (abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])))
-                        k = j
-                        print(i , j, y)
-                d.append(k)
-                print(y)
+            y = float("inf")
+            k = 0
+            for j in range(len(points)):
+                if  [i, j] in d or [j, i] in d:
+                    y = 0
+                    k = j
+                elif i!=j and y>(abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])):
+                    y = (abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]))
+                    k = j
+            d.append([i, k])
+            print(i, k, y)
+            if y!=float("inf"):
                 x += y
         print(x)
         return x
 
-minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]])
+minCostConnectPoints([[0,0],[1,1],[1,0],[-1,1]])
