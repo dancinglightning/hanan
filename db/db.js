fd90ee55f17689faa02402a0a54092ca34bfbdcf
Hanan Basheer <20b030018@iitb.ac.in>
2022-04-28 03:16:07 +0530

    Final

--- db/db.js
+++ db/db.js
@@ -187,27 +187,18 @@ export default {
 		],
 		freelance: [
 			{
-				projectName: "SnylloAir.com",
-				image: "images/snylloair.png",
+				projectName: "Hanculus - Discrete Calculus",
+				image: "images/hanculus.jpg",
 				summary:
-					"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-				preview: "https://www.snylloair.com/",
-				techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
-			},
-			{
-				projectName: "Delivery+",
-				image: "images/AM-Logo-.png",
-				summary:
-					"Android Application to display website in android devices.",
-				preview:
-					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
-				techStack: ["Android", "JAVA", "Play Store"],
+					"Developed a paper on Discrete form of Calculus in attempt to solve Reimann Integral Problem and was later disclosed as field of complex maths known as finite differences by Mike Giles, Professor of Scientific Computing Head of Department of Mathematical Institute University of Oxford.",
+				preview: "https://drive.google.com/drive/u/0/folders/1T8PMh67d3SwjMK7Q5ztCIk1dz_E7OeK7",
+				techStack: ["Calculus", "Math"],
 			},
 		],
 	},
 	experience: [
 		{
-			title: "Novopay Solutions Pvt. Ltd.",
+			title: "Unmesh Mashruwala Innovation Cell",
 			duration: "June 2020 - Present",
 			subtitle: "Software Engineer, SDE Intern",
 			details: [
