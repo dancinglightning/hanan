288897cdc654a791e64a485f37cf6b11875ddc32
Hanan Basheer <20b030018@iitb.ac.in>
2022-06-05 07:04:20 +0530

    Revert "Revamp"
    This reverts commit 6063fefe42cd86d0615bf0cbeb6e229407f5e4da.

---
+++ db/db.js
@@ -0,0 +1,429 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Hanan Basheer, an Aerospace undergrad at IIT Bombay",
+				"I'm a Developer & Competitive Programmer. You can often find me pondering over various unsolved and peculiar problems like Reimann Integral, Game of Life......",
+				"Solving mysteries and problem solving are things which I thrive on :p",
+				"I love to play chess, e-Games like Valorant & FIFA. Also a sport enthusiast, with likes of Football (Die-Hard Neymar Fan) and Table Tennis. Extremely passionate about swimming as well.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: hanubhai2001@gmail.com",
+			],
+		},
+	},
+	specifics: [
+		{
+			title: "Python",
+			skillName: "Python",
+			color: "1",
+			percentage: "75",
+		},
+		{
+			title: "C++",
+			skillName: "C++",
+			color: "2",
+			percentage: "50",
+		},
+		{
+			title: "HTML",
+			skillName: "HTML",
+			color: "3",
+			percentage: "35",
+		},
+		{
+			title: "LaTeX",
+			skillName: "LaTeX",
+			color: "4",
+			percentage: "45",
+		},
+		{
+			title: "Octave",
+			skillName: "OCtave",
+			color: "5",
+			percentage: "40",
+		},
+		{
+			title: "Jupyter Notebook",
+			skillName: "Jupyter Notebook",
+			color: "6",
+			percentage: "55",
+		},
+		{
+			title: "Arduino IDE",
+			skillName: "Arduino IDE",
+			color: "7",
+			percentage: "50",
+		},
+		{
+			title: "Matlab",
+			skillName: "Matlab",
+			color: "8",
+			percentage: "30",
+		},
+		{
+			title: "Django",
+			skillName: "Django",
+			color: "9",
+			percentage: "30",
+		},
+		{
+			title: "Dart",
+			skillName: "Dart",
+			color: "1",
+			percentage: "40",
+		},
+		{
+			title: "Github/Git",
+			skillName: "Github/Git",
+			color: "2",
+			percentage: "80",
+		},
+		{
+			title: "ROS",
+			skillName: "ROS",
+			color: "3",
+			percentage: "60",
+		},
+		{
+			title: "SQL",
+			skillName: "SQL",
+			color: "4",
+			percentage: "30",
+		},
+		{
+			title: "VS Code",
+			skillName: "VS Code",
+			color: "5",
+			percentage: "80",
+		},
+		{
+			title: "Ansys",
+			skillName: "Ansys",
+			color: "6",
+			percentage: "30",
+		},
+		{
+			title: "Excel",
+			skillName: "Excel",
+			color: "7",
+			percentage: "80",
+		},
+	],
+	skills: [
+		{
+			title: "Python",
+			skillName: "Python",
+			color: "1",
+			percentage: "75",
+		},
+		{
+			title: "C++",
+			skillName: "C++",
+			color: "2",
+			percentage: "50",
+		},
+		{
+			title: "HTML",
+			skillName: "HTML",
+			color: "3",
+			percentage: "35",
+		},
+		{
+			title: "LaTeX",
+			skillName: "LaTeX",
+			color: "4",
+			percentage: "45",
+		},
+		{
+			title: "Octave",
+			skillName: "OCtave",
+			color: "5",
+			percentage: "40",
+		},
+		{
+			title: "Jupyter Notebook",
+			skillName: "Jupyter Notebook",
+			color: "6",
+			percentage: "55",
+		},
+		{
+			title: "Arduino IDE",
+			skillName: "Arduino IDE",
+			color: "7",
+			percentage: "50",
+		},
+		{
+			title: "Matlab",
+			skillName: "Matlab",
+			color: "8",
+			percentage: "30",
+		},
+		{
+			title: "Django",
+			skillName: "Django",
+			color: "9",
+			percentage: "30",
+		},
+		{
+			title: "Dart",
+			skillName: "Dart",
+			color: "1",
+			percentage: "40",
+		},
+		{
+			title: "Github/Git",
+			skillName: "Github/Git",
+			color: "2",
+			percentage: "80",
+		},
+		{
+			title: "ROS",
+			skillName: "ROS",
+			color: "3",
+			percentage: "60",
+		},
+		{
+			title: "SQL",
+			skillName: "SQL",
+			color: "4",
+			percentage: "30",
+		},
+		{
+			title: "VS Code",
+			skillName: "VS Code",
+			color: "5",
+			percentage: "80",
+		},
+		{
+			title: "Ansys",
+			skillName: "Ansys",
+			color: "6",
+			percentage: "30",
+		},
+		{
+			title: "Excel",
+			skillName: "Excel",
+			color: "7",
+			percentage: "80",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "Hand of God",
+				image: "images/hand.jpg",
+				summary:
+					"Developed a controller glove which can be used to control daily life appliances as well as gadgets with IoT integrations. It was the winning project of Institute Technical Summer Project (ITSP) '21.",
+				preview: "https://github.com/dancinglightning/ITSP-21_HAND-OF-GOD",
+				techStack: [
+					"Python",
+					"C++",
+					"Arduino IDE",
+					"String Sensors",
+					"Tkinter",
+				],
+			},
+			{
+				projectName: "Sedrica - Self Driving Car",
+				image: "images/sedrica.png",
+				summary:
+					"Developed and implemented control code for self-driving car, Sedrica, which is one of the key projects of Innovation Cell of IIT-B. Control code basically invokes commands to various control units of car as it moves tracing its route. We participate in IGVC which happens in Oakland University in Michigan, USA every year.",
+				preview: "https://github.com/dancinglightning/IGVC_2022_Controls",
+				techStack: ["Python", "ROS", "Gazebo", "Self-driving Car"],
+			},
+		],
+		software: [
+			{
+				projectName: "Lasso Game Development",
+				image: "images/lassogame.png",
+				summary:
+					"A Lasso game where your objective is to catch falling coins and with multiple variants and game modes.",
+				preview:
+					"https://github.com/dancinglightning/IIT-B/tree/main/CS%20101",
+				techStack: ["C++", "Object-Oriented Programming"],
+			},
+			{
+				projectName: "Hybrid ANN-Statistical Model for Robust Stock Market Prediction",
+				image: "images/stockmarket.jpg",
+				summary:
+					"Python script which predicts Stock market price flow based on GARCH and ANN model.",
+				preview: "https://github.com/dancinglightning/SoC-21-Stock-Market",
+				techStack: ["Stock Market", "ANN", "Python"],
+			},
+			{
+				projectName: "Spanning Tree Protocol",
+				image: "images/spanningtreeprotocol.png",
+				summary:
+					"Code to automate Spanning tree protocol to generate type of ports to set up a network.",
+				preview: "https://github.com/dancinglightning/IIT-B/tree/main/CS%20224/Spanning-Tree-Protocol",
+				techStack: ["C++", "Networks", "Information Exhange"],
+			},
+		],
+		android: [
+			{
+				projectName: "Mobile Calculator Application",
+				image: "images/calculator.jpg",
+				summary:
+					"Successfully created a completely working Calculator app using Dart on Flutter and Android Studio.",
+				preview: "https://github.com/dancinglightning/20B030018_IntroToAppDev/tree/main/Week%201",
+				techStack: ["DART", "Flutter", "Android Studio"],
+			},
+			{
+				projectName: "Quiz Application",
+				image: "images/quiz.jpg",
+				summary:
+					"A quiz application with database of multiple True/False questions based on daily activities as well as general knowledge.",
+				preview: "https://github.com/dancinglightning/20B030018_IntroToAppDev/tree/main/Week%202",
+				techStack: ["DART", "Flutter", "Android Studio"],
+			},
+			{
+				projectName: "TO-DO Application",
+				image: "images/to-do.jpg",
+				summary:
+					"A TO-DO list app with integrated Google Firebase authentication to the app for login and security purpose.",
+				preview: "https://github.com/dancinglightning/20B030018_IntroToAppDev/tree/main/Week%204",
+				techStack: ["DART", "Flutter", "Android Studio", "Google Firebase"],
+			},
+		],
+		freelance: [
+			{
+				projectName: "Hanculus - Discrete Calculus",
+				image: "images/hanculus.jpg",
+				summary:
+					"Developed a paper on Discrete form of Calculus in attempt to solve Reimann Integral Problem and responded by Mike Giles, Professor of Scientific Computing Head of Department of Mathematical Institute University of Oxford.",
+				preview: "https://drive.google.com/drive/u/0/folders/1T8PMh67d3SwjMK7Q5ztCIk1dz_E7OeK7",
+				techStack: ["Calculus", "Math"],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "Unmesh Mashruwala Innovation Cell",
+			duration: "May 2022 - Present",
+			subtitle: "Sedrica Controls Subsystem lead & UMIC manager",
+			details: [
+				"Promoted to Sedrica Controls Subsystem Lead and UMIC manager.",
+			],
+			tags: [
+				"Managment",
+				"ROS",
+				"Python",
+				"C++",
+				"Gazebo",
+				"Control Theory",
+				"SysCon"
+			],
+			icon: "fa fa-group",
+		},
+		{
+			title: "Unmesh Mashruwala Innovation Cell",
+			duration: "September 2021 - May 2022",
+			subtitle: "Controls Engineer",
+			details: [
+				"Controls engineer of Team Sedrica which is going to participate in IGVC in upcoming year.",
+				"Designed code for implementation of NMPC time independent algorithm as a substitute to older and clogged version of code.",
+			],
+			tags: [
+				"ROS",
+				"Python",
+				"C++",
+				"Gazebo",
+				"Control Theory",
+				"SysCon"
+			],
+			icon: "fas fa-car",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors of Technology in Aerospace with Minors in Computer Science",
+			duration: "4 years",
+			subtitle: "Indian Institute of Technology, Bombay (IIT-B)",
+			details: [
+				"Completing B.Tech in Aerospace (2/4 years completed)",
+				"Completing Minors in Computer Science (2/5 courses done)",
+			],
+			tags: [
+				"Aerospace", "Aeronautics",
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Computer Networks",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "High School",
+			duration: "2 years",
+			subtitle: "Delhi Public School, Navi Mumbai",
+			details: [
+				"Qualified JEE Advanced, JEE MAINS & BITSAT",
+				"Secured 94.6 percentage in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics", "Computer Science"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "Google Developer Profile",
+					link: "https://developers.google.com/profile/u/dancinglightning",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/dancinglightning",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/dancinglightning/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				{
+					text: "My Resume",
+					link: "https://drive.google.com/file/d/12_dXYiWSvps-nybG2gwhqLIfm82WVTvF/view?usp=sharing",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/hanan-basheer-936b62200/",
+				},
+				{
+					text: "Instagram",
+					link: "https://www.instagram.com/__dancing_lightning__/",
+				},
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/hananbasheer",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with ❤️ by Hanan Basheer."
+			],
+		},
+	],
+};
